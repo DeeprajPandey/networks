0a47149863d232283554f7f0084c0e7c083a352a
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2019-11-09 13:33:31 +0530

    Password checks and new password generation (diceware) plus some refactoring

--- A4P3/readx.py
+++ A4P3/readx.py
@@ -4,22 +4,37 @@ import numpy as np
 import math
 # for password generation
 import random,sys
+# passwords stored in json file
+import json
 
-### Generate an n-word diceware password ###
-# wordlist is the EFF diceware long word list
-wordlist = [ln.split()[1] for ln in open('./wordlist')]
+
+### Globals ###
+# username & password from client
+name = 'rathi.kashi_ug20'
+entered_pass = 'frittletop'
+
+# store what the last test that the student took
+test_label = np.nan
+passwords = {}
+
+# all passwords in a dict
+with open("passwords.json", 'r') as istream:
+	passwords = json.load(istream)
+
+# read the excel file and import data as a dataframe
+data = pd.read_excel('scores.xlsx', 'monsoon19', header=0)
+
+
+### Function to generate an n-word diceware password ###
 def get_passphrase(n):
-	# use the wordlist array we made above
-	global wordlist
+	# wordlist is the EFF diceware long word list
+	wordlist = [ln.split()[1] for ln in open('./wordlist')]
 	# `n` words joined by -
 	pp = '-'.join(random.SystemRandom().choice(wordlist) for i in range(n))
 	return pp
 
 
-# store what the last test that the student took
-test_label = np.nan
-
-# returns the last valid test score
+### Returns the last valid test score in dfObject ###
 # and stores the test label in the global test_label variable
 def last_valid(y):
 	global test_label
@@ -30,9 +45,6 @@ def last_valid(y):
 		test_label = y.last_valid_index()
 		return y[y.last_valid_index()]
 
-# username & password from client
-name = 'rathi.kashi_ug20'
-entered_pass = 'frittletop'
 
 # check if the user entered anything
 if math.isnan(name) or math.isnan(entered_pass):
@@ -41,35 +53,36 @@ if math.isnan(name) or math.isnan(entered_pass):
 	been assigned to you on your first login on this system.\
 	Please contact your course faculty if you have lost your password."
 
-
-### read the excel file and import data as dataframes ###
-data = pd.read_excel('scores.xlsx', 'monsoon19', header=0)
-# usernames are indices in this data frame
-passwords = pd.read_excel('scores.xlsx', 'passwords', header=0, index_col=0)
-
-# returns an array of true(for matches)/false(if not)
+# str.match() returns an array of true(for matches)/false(if not)
 indices = data['Students'].str.match(name)
 # dataframe with rows where the student column matches the username
 search_results  = data[indices]
 
-# if the name wasn't found in monsoon19
-if search_results.empty:
+# if the name wasn't found in monsoon19 or password list
+if search_results.empty or\
+name not in passwords.keys:
 	print("Invalid username or student not in the course. Try again!")
 	# continue listening for usernames until sees 'exit'
 
 # df.shape gives a (row,col) tuple
 # if the password is correct and more than one row in results
-elif search_results.shape[0] > 1 and\
-	 entered_pass == passwords.loc[name]['stored']:
+elif entered_pass == passwords[name] and\
+search_results.shape[0] > 1:
 	print("There are duplicate records for this username. Contact your faculty.")
 	# continue listening for usernames until sees 'exit'
 
 # one match for `username` found
 else:
-	if passwords.loc[name]['stored'] == 'not set':
+	if passwords[name] == "not set":
 		# call pass_generator() and store it in the sheet
-		# print it to the user
-	elif entered_pass == passwords.loc[name]['stored']:
+		new_pass = pass_generator(5)
+		passwords[name] = new_pass
+		# print it for the user
+		print "Your password is", new_pass
+		print "Store it in a secure place. If you forget your password, contact course faculty."
+		# we don't need this sticking around anymore
+		new_pass = np.nan
+	elif entered_pass == passwords[name]:
 		# array with row index and value
 		latest_score = search_results.apply(last_valid, axis=1)
 		print "Your score in", test_label, "is " + str(latest_score[1]) + "."
@@ -79,3 +92,7 @@ else:
 	else:
 		print "Incorrect password. Try again."
 		# continue listening for usernames until sees 'exit'
+
+# dump all updates passwords to the json file
+with open("passwords.json", 'w') as ostream:
+	json.dump(passwords, ostream)
