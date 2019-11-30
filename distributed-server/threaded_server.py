38c990d3bdcb5e15b618ed192817a2a54534faa5
Reuel John <reueljohn@protonmail.com>
2019-11-30 18:02:18 +0530

    added sync mode

--- distributed-server/threaded_server.py
+++ distributed-server/threaded_server.py
@@ -2,16 +2,24 @@ import socket as skt
 from _thread import*
 import threading
 from datetime import datetime
+import pickle
+import schedule
 
-request_queue = list()
+request_queue = []
 nums = list([3, 9, 6, 1, 10, 5, 2, 7, 4, 8])
 
-IP = skt.gethostname()
+HOST_IP = skt.gethostname()
+S1_IP = "10.1.0.100"
+S2_IP = "10.1.0.100"
 PORT = 8000
 
 serversocket = skt.socket(skt.AF_INET, skt.SOCK_STREAM)
 serversocket.setsockopt(skt.SOL_SOCKET, skt.SO_REUSEADDR, 1)
 serversocket.bind((IP, PORT))
+
+
+clientsocket = skt.socket(skt.AF_INET, skt.SOCK_STREAM)
+
 
 def swap(i, j):
     if i < 10 and j < 10:
@@ -24,19 +32,18 @@ def swap(i, j):
 # Use a re-entrant lock in case same thread tries to acquire lock multiple times
 q_lock = threading.RLock()
 
-def update_nums(rq1, rq2):
+def update_nums(rq1):
 
     request_queue.append(rq1)
-    request_queue.append(rq2)
 
     request_queue = sorted(request_queue, key=itemgetter(1))
-
     for i in range(len(request_queue)):
         tuple_to_swap = i[0]
         temp = nums[tuple_to_swap[0]]
         nums[tuple_to_swap[0]] = nums[tuple_to_swap[1]]
         nums[tuple_to_swap[1]] = temp
     request_queue=[]
+
     return nums
 
 def process_client_request(clientsock, addr):
@@ -68,19 +75,37 @@ def process_client_request(clientsock, addr):
     # returning from function kills the thread
     print("Thread closed. Current queue is: {}\n\n".format(str(request_queue)))
 
+#header and server ip
 def sync_mode():
-    
 
+    header="S1"
+    clientsocket.connect((S1_IP,PORT))
+    #send identifying that i am a server
+    clientsocket.send(header.encode())
+    #receive the requestq
+    s_rq=clientsocket.recv(1024).decode()
+    clientsocket.close()
+
+    header="S2"
+    clientsocket.connect((S2_IP,PORT))
+    #send identifying that i am a server
+    clientsocket.send(header.encode())
+    #receive the requestq
+    s_rq.append(clientsocket.recv(1024).decode())
+    clientsocket.close()
+
+    update_nums(s_rq)
 
 def Main():
     serversocket.listen(7)
     print("Server is listening...\n")
+    schedule.every(15).seconds.do(sync_mode())
 
+    #Start listening to clients
     while True:
         (client, address) = serversocket.accept()
         print("Connected to client ", address[0], ":", address[1])
         start_new_thread(process_client_request, (client, address))
-        
     serversocket.close()
 
 
